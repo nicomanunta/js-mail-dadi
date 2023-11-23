121a87b3e2b5b23c95e90ba567a37b5fe3e122a4
nicomanunta <nic.manun@gmail.com>
2023-11-23 17:10:25 +0100

    Esercizio mail finito

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,21 +1,28 @@
+// permettere di inserire una mail tramite prompt o input
 let mail = prompt("Inserire la propria mail")
-
+// creare un array contenente un tot di mail
 const list_mail = ["luca@mail.com", "giorgio@mail.com", "davide@mail.com", "michele@mail.com","marco@mail.com"]
 console.log(list_mail)
+// creare una variabile flag con valore falso
+let mail_trovata = false;
 
-
-let trovata_mail = true
-
+// scrivere un ciclo per verificare che la mail inserita sia presente nell'array
 for(let i = 0; i<list_mail.length; i++){
-    
 
+    // se presente, la variabile flag diventa vera
     if(mail == list_mail[i]){
-        let message = "Mail accettata"
-        console.log(message)
-    }
-    else{
-        let message = "Mail rifiutata"
-        console.log(message)
+
+        mail_trovata = true
     }
 
+}
+// fuori dal ciclo far apparire un messaggio
+// se la variabile flag è diventata vera appare "Entra"
+if (mail_trovata == true){
+    console.log("Entra")
+
+}
+// altrimenti appare "stai fuori"
+else{
+    console.log("Stai fuori")
 }
